eac94875f20e7d8a3ddebe15db5448ee9b8e0de0
vagrant <vagrant@localhost.localdomain>
2020-10-26 15:53:18 +0900

    for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,5 +18,3 @@ User.create(
   email: Rails.application.credentials.user[:email1],
   password: Rails.application.credentials.user[:password1]
 )
-
-Entry.create(user_id: 1, title: 'SHIROBAKO', introduction: 'SHIROBAKOのレビューです！')
